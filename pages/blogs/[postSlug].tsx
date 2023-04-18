1b369374b49c0b8ff8521b62a84ae0615d23eeff
Josey Kinnaman <josey.kinnaman@themomproject.com>
2023-04-18 15:14:36 -0700

    Add static paths and props for blogs

--- pages/blogs/[postSlug].tsx
+++ pages/blogs/[postSlug].tsx
@@ -1,14 +1,65 @@
-import { GetStaticProps, NextPage } from 'next';
+import { GetStaticProps, GetStaticPaths, NextPage, InferGetStaticPropsType } from 'next';
+import fs from 'fs';
+import path from 'path';
+import matter from "gray-matter";
+import { ParsedUrlQuery } from 'querystring';
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
 
 
-interface Props {}
 
-const SinglePage: NextPage<Props> = () => {
-  return <div>SinglePage</div>;
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+const SinglePage: NextPage<Props> = ({ post }) => {
+  const {content, title } = post
+  return <div>
+    <h1>{title}</h1>
+    <MDXRemote {...content} />
+  </div>
 };
-export const getStaticProps: GetStaticProps = () => {
+
+export const getStaticPaths : GetStaticPaths = ()=> {
+  //reading paths
+  const dirPathToRead = path.join(process.cwd(), 'posts')
+  const dirs = fs.readdirSync(dirPathToRead)
+  const paths = dirs.map((filename) => {
+  const filePathToRead = path.join(process.cwd(), "posts/" + filename);
+  const fileContent = fs.readFileSync(filePathToRead, {encoding: "utf-8"});
+  return {params: {postSlug: matter(fileContent).data.slug } }
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+interface IStaticProps extends ParsedUrlQuery{
+  postSlug: string
+}
+
+type Post = {
+  post: {
+    title: string;
+    content: MDXRemoteSerializeResult;
+  }
+}
+
+export const getStaticProps: GetStaticProps<Post> = async(context) =>
+{
+  const { params } = context;
+  const { postSlug } = params as IStaticProps;
+  const filePathToRead = path.join(process.cwd(), "posts/" + postSlug + '.md');
+  const fileContent = fs.readFileSync(filePathToRead, {encoding: "utf-8"});
+
+  const source: any = await serialize(fileContent, {parseFrontmatter: true});
   return{
-    props: {},
+    props: {
+      post: {
+        content: source,
+        title: source.frontmatter.title,
+      },
+    },
   };
 };
 
